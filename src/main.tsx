2cae792d887dbc81c33313f7dafe11d0e1289f35
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2023-11-26 12:46:17 -0300

    feat: web cache configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import './styles/globals.css'
 
 import React from 'react'
 import * as Router from 'react-router-dom'
+import * as ReactQuery from 'react-query'
 import ReactDOM from 'react-dom/client'
 
+import { queryClient } from './services/queryClient'
 import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router.RouterProvider router={router} />
+    <ReactQuery.QueryClientProvider client={queryClient}>
+      <Router.RouterProvider router={router} />
+    </ReactQuery.QueryClientProvider>
   </React.StrictMode>
 )
