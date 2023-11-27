1fde24273a0deb8c1992b30f3942781685c2346b
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2023-11-26 22:18:01 -0300

    style: adjustments to responsiveness in mobile mode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -51,121 +51,130 @@ export const Home: React.FC<IProps> = () => {
     <div className="min-h-screen pb-28 bg-background">
       <Header subtitle="Uma janela para o mundo dos desenvolvedores." title="GitLister" />
 
-      {(!data?.user || !user) && (
-        <div className="grid gap-12 md:grid-cols-[1fr_auto] max-w-5xl w-full mx-auto mt-16 p-12 rounded-md bg-white">
-          <p className="text-base text-purple">
-            Descubra projetos inovadores, acompanhe as tendências de código e conecte-se com a comunidade global de
-            criadores. Mergulhe na diversidade de repositórios e veja a magia da colaboração acontecer em tempo real.
-          </p>
+      <div className="px-2">
+        {(!data?.user || !user) && (
+          <div className="grid gap-12 md:grid-cols-[1fr_auto] max-w-5xl w-full mx-auto mt-16 p-12 rounded-md bg-white">
+            <p className="text-base text-purple">
+              Descubra projetos inovadores, acompanhe as tendências de código e conecte-se com a comunidade global de
+              criadores. Mergulhe na diversidade de repositórios e veja a magia da colaboração acontecer em tempo real.
+            </p>
 
-          <div className="grid gap-8">
-            <Input.Container>
-              <Input.Label text="Usuário" />
-              <Input.Field placeholder="Digite o nome do suário" onChange={event => setUsername(event.target.value)} />
-            </Input.Container>
-
-            <Button disabled={isFetching} type="button" onClick={() => refetch()}>
-              {isFetching ? 'Buscando...' : 'Buscar'}
-            </Button>
-          </div>
-        </div>
-      )}
-
-      {isFetched && data?.user && !!user && (
-        <>
-          <div className="grid gap-6 max-w-5xl w-full mx-4 md:mx-auto mt-16 p-4 md:p-12 rounded-md bg-white text-purple">
-            <div className="grid gap-6 grid-cols-[auto_1fr]">
-              <div className="flex justify-center items-center w-32 h-32 md:w-40 md:h-40 bg-background rounded-full">
-                <img
-                  className="w-28 h-28 md:w-36 md:h-36 rounded-full"
-                  alt={`Foto do usuário ${data?.user.name}.`}
-                  src={data?.user.avatar_url}
+            <div className="grid gap-8">
+              <Input.Container>
+                <Input.Label text="Usuário" />
+                <Input.Field
+                  placeholder="Digite o nome do suário"
+                  onChange={event => setUsername(event.target.value)}
                 />
-              </div>
+              </Input.Container>
 
-              <div className="flex justify-between flex-col md:flex-row">
-                <div>
-                  <a href={data.user.html_url} target="_blank" rel="noopener noreferrer">
-                    <h2 className="font-medium text-3xl mt-4 hover:underline cursor-pointer">{data?.user.name}</h2>
-                  </a>
-
-                  <p className="text-md">
-                    <strong>Username: </strong>
-                    {data?.user.login}
-                  </p>
-                </div>
-
-                <div className="flex flex-col md:items-end">
-                  <span className="flex items-center">
-                    <FiUsers className="mr-2" size={18} /> {data?.user.followers} seguidores | {data?.user.following}{' '}
-                    seguindo | {data?.user.public_repos} repositórios.
-                  </span>
-
-                  {data?.user.email && (
-                    <span className="flex items-center">
-                      <FiMail className="mr-2" size={18} />
-                      <a className="text-md" href={`mailto:${data?.user.email}`}>
-                        {data?.user.email}
-                      </a>
-                    </span>
-                  )}
-
-                  {data?.user.location && (
-                    <span className="flex items-center">
-                      <FiMapPin className="mr-2" size={18} /> {data?.user.location}
-                    </span>
-                  )}
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <h3 className="text-xl font-bold mb-2">Descrição</h3>
-              <p>{data?.user.bio || 'Nenhuma descrição foi fornecida.'}</p>
-            </div>
-
-            <div className="mx-auto mt-12">
-              <Button onClick={removeUser} type="button">
-                Efetuar nova busca
+              <Button disabled={isFetching} type="button" onClick={() => refetch()}>
+                {isFetching ? 'Buscando...' : 'Buscar'}
               </Button>
             </div>
           </div>
+        )}
 
-          <div className="max-w-5xl w-full mx-4 md:mx-auto mt-8 p-4 md:p-12 rounded-md bg-white text-purple">
-            <div className="flex justify-between items-center mb-12">
-              <h2 className="font-medium text-3xl">Lista de repositórios</h2>
+        {isFetched && data?.user && !!user && (
+          <>
+            <div className="grid gap-6 max-w-5xl w-full md:mx-auto mt-16 p-4 md:p-12 rounded-md bg-white text-purple">
+              <div className="grid gap-6 grid-cols-[auto_1fr]">
+                <div className="flex justify-center items-center w-32 h-32 md:w-40 md:h-40 bg-background rounded-full">
+                  <img
+                    className="w-28 h-28 md:w-36 md:h-36 rounded-full"
+                    alt={`Foto do usuário ${data?.user.name}.`}
+                    src={data?.user.avatar_url}
+                  />
+                </div>
 
-              <Button className="w-28 h-12" onClick={reorderRepositories} type="button">
-                Ordenar {order === 'ASC' ? <FiChevronUp size={22} /> : <FiChevronDown size={22} />}
-              </Button>
-            </div>
+                <div className="flex justify-between flex-col md:flex-row">
+                  <div>
+                    <a href={data.user.html_url} target="_blank" rel="noopener noreferrer">
+                      <h2 className="font-medium text-3xl mt-4 hover:underline cursor-pointer">{data?.user.name}</h2>
+                    </a>
 
-            <div className="grid gap-2">
-              {data?.repositories?.map(repository => (
-                <div key={repository.id} className="grid grid-cols-[1fr_6rem] border rounded-md">
-                  <div className="p-2">
-                    <h4 className="font-semibold text-xl">{repository.name}</h4>
-
-                    <div className="flex">
-                      <p className="text-base">{repository.language}</p>
-
-                      <span className="flex items-center ml-4">
-                        <FiStar className="mr-1 fill-amber-500" size={18} /> {repository.stargazers_count}
-                      </span>
-                    </div>
+                    <p className="text-md">
+                      <strong>Username: </strong>
+                      {data?.user.login}
+                    </p>
                   </div>
 
-                  <NavLink to={`/repository/${repository.full_name}`}>
-                    <Button className="rounded-l-none h-full w-full" type="button">
-                      Visualizar
-                    </Button>
-                  </NavLink>
+                  <div className="flex flex-col md:items-end">
+                    <div className="md:flex md:justify-between">
+                      <span className="flex items-center mr-2">
+                        <FiUsers className="mr-2" size={18} /> {data?.user.followers} seguidores
+                      </span>
+
+                      <p className="mr-2">{data?.user.following} seguindo</p>
+                      <p> {data?.user.public_repos} repositórios</p>
+                    </div>
+
+                    {data?.user.email && (
+                      <span className="flex items-center">
+                        <FiMail className="mr-2" size={18} />
+                        <a className="text-md" href={`mailto:${data?.user.email}`}>
+                          {data?.user.email}
+                        </a>
+                      </span>
+                    )}
+
+                    {data?.user.location && (
+                      <span className="flex items-center">
+                        <FiMapPin className="mr-2" size={18} /> {data?.user.location}
+                      </span>
+                    )}
+                  </div>
                 </div>
-              ))}
+              </div>
+
+              <div>
+                <h3 className="text-xl font-bold mb-2">Descrição</h3>
+                <p>{data?.user.bio || 'Nenhuma descrição foi fornecida.'}</p>
+              </div>
+
+              <div className="mx-auto mt-12">
+                <Button onClick={removeUser} type="button">
+                  Efetuar nova busca
+                </Button>
+              </div>
             </div>
-          </div>
-        </>
-      )}
+
+            <div className="max-w-5xl w-full md:mx-auto mt-8 p-4 md:p-12 rounded-md bg-white text-purple">
+              <div className="flex justify-between items-center mb-12">
+                <h2 className="font-medium text-3xl">Lista de repositórios</h2>
+
+                <Button className="w-28 h-12" onClick={reorderRepositories} type="button">
+                  Ordenar {order === 'ASC' ? <FiChevronUp size={22} /> : <FiChevronDown size={22} />}
+                </Button>
+              </div>
+
+              <div className="grid gap-2">
+                {data?.repositories?.map(repository => (
+                  <div key={repository.id} className="grid grid-cols-[1fr_6rem] border rounded-md">
+                    <div className="p-2">
+                      <h4 className="font-semibold text-xl">{repository.name}</h4>
+
+                      <div className="flex">
+                        <p className="text-base">{repository.language}</p>
+
+                        <span className="flex items-center ml-4">
+                          <FiStar className="mr-1 fill-amber-500" size={18} /> {repository.stargazers_count}
+                        </span>
+                      </div>
+                    </div>
+
+                    <NavLink to={`/repository/${repository.full_name}`}>
+                      <Button className="rounded-l-none h-full w-full" type="button">
+                        Visualizar
+                      </Button>
+                    </NavLink>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -32,53 +32,55 @@ export const Repository: React.FC<IProps> = () => {
     <div className="min-h-screen pb-28 bg-background">
       <Header subtitle="Uma janela para o mundo dos desenvolvedores." title="GitLister" />
 
-      <div className="grid max-w-5xl w-full mx-4 md:mx-auto mt-16 p-4 md:p-12 rounded-md bg-white text-purple">
-        {status === 'loading' && <span className="mx-auto py-36 text-lg font-semibold">Carregando dados...</span>}
-        {!!errorMessage && <span className="mx-auto py-36 text-lg font-semibold">{errorMessage}</span>}
+      <div className="px-2">
+        <div className="grid max-w-5xl w-full md:mx-auto mt-16 p-4 md:p-12 rounded-md bg-white text-purple">
+          {status === 'loading' && <span className="mx-auto py-36 text-lg font-semibold">Carregando dados...</span>}
+          {!!errorMessage && <span className="mx-auto py-36 text-lg font-semibold">{errorMessage}</span>}
 
-        {status === 'success' && !!data?.repository && (
-          <>
-            <div className="flex justify-between mb-2">
-              <a href={data.repository.html_url} target="_blank" rel="noopener noreferrer">
-                <h2 className="font-medium text-3xl mt-4 hover:underline cursor-pointer">{data.repository.name}</h2>
-              </a>
+          {status === 'success' && !!data?.repository && (
+            <>
+              <div className="flex justify-between mb-2">
+                <a href={data.repository.html_url} target="_blank" rel="noopener noreferrer">
+                  <h2 className="font-medium text-3xl mt-4 hover:underline cursor-pointer">{data.repository.name}</h2>
+                </a>
 
-              <div className="flex">
-                <span className="flex items-center ml-4">
-                  <FiStar className="mr-1 fill-amber-500" size={18} /> {data.repository.stargazers_count}
-                </span>
+                <div className="flex">
+                  <span className="flex items-center ml-4">
+                    <FiStar className="mr-1 fill-amber-500" size={18} /> {data.repository.stargazers_count}
+                  </span>
 
-                <span className="flex items-center ml-4">
-                  <FiEye className="mr-1 fill-amber-500" size={18} /> {data.repository.watchers_count}
-                </span>
+                  <span className="flex items-center ml-4">
+                    <FiEye className="mr-1 fill-amber-500" size={18} /> {data.repository.watchers_count}
+                  </span>
+                </div>
               </div>
-            </div>
 
-            {data.repository.topics.length > 0 && (
-              <p>
-                <strong>Tópicos: </strong>
-                {data.repository.topics.map((item, index) => (index === 0 ? item : `, ${item}`))}.
+              {data.repository.topics.length > 0 && (
+                <p>
+                  <strong>Tópicos: </strong>
+                  {data.repository.topics.map((item, index) => (index === 0 ? item : `, ${item}`))}.
+                </p>
+              )}
+
+              <p className="text-base">
+                <strong>Linguagem: </strong>
+                {data.repository.language}
               </p>
-            )}
 
-            <p className="text-base">
-              <strong>Linguagem: </strong>
-              {data.repository.language}
-            </p>
+              <div className="my-12">
+                <h3 className="text-xl font-bold mb-2">Descrição</h3>
+                <p>{data.repository.description || 'Nenhuma descrição foi fornecida.'}</p>
+              </div>
+            </>
+          )}
 
-            <div className="my-12">
-              <h3 className="text-xl font-bold mb-2">Descrição</h3>
-              <p>{data.repository.description || 'Nenhuma descrição foi fornecida.'}</p>
-            </div>
-          </>
-        )}
-
-        <div className="mx-auto">
-          <NavLink to="/">
-            <Button className="" type="button">
-              Voltar
-            </Button>
-          </NavLink>
+          <div className="mx-auto">
+            <NavLink to="/">
+              <Button className="" type="button">
+                Voltar
+              </Button>
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
